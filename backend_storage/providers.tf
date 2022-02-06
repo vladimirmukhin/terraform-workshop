c10e14a7ab5861313f5712c41aa521a2689f478f
Vladimir Mukhin <awsvlad@gmail.com>
2022-02-06 15:40:44 -0500

    Create S3 bucket and DynamoDB table

---
+++ backend_storage/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+ default_tags {
+   tags = {
+     Owner       = "Vladimir"
+     Project     = "Terraform-Workshop"
+   }
+ }
+}
