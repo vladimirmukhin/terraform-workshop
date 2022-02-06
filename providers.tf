7d1e6bbc15d5a1158d4ae3e59dac3a3df6e05cd6
Vladimir Mukhin <awsvlad@gmail.com>
2022-02-06 15:40:59 -0500

    Configure AWS provider to use remote state

--- providers.tf
+++ providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket         = "terraform-remote-state-vladimir"
+    key            = "main/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
